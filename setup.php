4f174408456153b8259a0feb4eafdfc78fdfd7bf
tsmr <thetsmr@gmail.com>
2022-07-09 17:33:09 +0200

    Bump version

--- setup.php
+++ setup.php
@@ -27,7 +27,7 @@
  @since     2009
  ---------------------------------------------------------------------- */
 
-define('PLUGIN_PURCHASEREQUEST_VERSION', '3.0.0');
+define('PLUGIN_PURCHASEREQUEST_VERSION', '3.0.1');
 
 if (!defined("PLUGIN_PURCHASEREQUEST_DIR")) {
    define("PLUGIN_PURCHASEREQUEST_DIR", Plugin::getPhpDir("purchaserequest"));
